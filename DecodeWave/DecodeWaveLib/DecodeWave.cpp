58a3989dc54de619c6dfe5ddbec516ac4017e3be
Charan DSouza <godwincharan@gmail.com>
2020-02-17 20:31:29 +0100

    Added test case for proper file path

--- DecodeWave/DecodeWaveLib/DecodeWave.cpp
+++ DecodeWave/DecodeWaveLib/DecodeWave.cpp
@@ -1,4 +1,5 @@
 #include "DecodeWave.hpp"
+#include "Logger.hpp"
 #define DR_WAV_IMPLEMENTATION
 #include "dr_wav.h"
 
@@ -17,7 +18,14 @@ bool DecodeWave::OpenFile(std::string& file_name)
     if(!file_name.empty())
     {
         drwav wav_;
-        if (drwav_init_file(&wav_, file_name.c_str(), nullptr)) {
+        if (drwav_init_file(&wav_, file_name.c_str(), nullptr)) 
+        {
+            std::cout << "Channels: " << wav_.channels << std::endl;
+            if (2 == wav_.channels)
+            {
+                return true;
+            }
+            
         }
     }
     return false;
