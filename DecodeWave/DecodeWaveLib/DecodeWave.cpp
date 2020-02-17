90139910b5fa30356c4fb550b25cbf5dc77681a1
Charan DSouza <godwincharan@gmail.com>
2020-02-17 20:58:07 +0100

    Added code for reading data.

--- DecodeWave/DecodeWaveLib/DecodeWave.cpp
+++ DecodeWave/DecodeWaveLib/DecodeWave.cpp
@@ -22,9 +22,23 @@ bool DecodeWave::OpenFile(std::string& file_name)
         if (drwav_init_file(&wav_, file_name.c_str(), nullptr)) 
         {
             logger::Log::Get().log(std::string("Channels: ") + std::to_string(wav_.channels));
+            logger::Log::Get().log(std::string("Sample Rate: ") + std::to_string(wav_.sampleRate));
+            logger::Log::Get().log(std::string("Total Samples: ") + std::to_string(wav_.totalPCMFrameCount));
             if (2 == wav_.channels)
             {
-                result = true;
+               
+
+                auto total_data = wav_.totalPCMFrameCount * wav_.channels;
+                int32_t* pSampleData = (int32_t*)malloc((size_t)total_data * sizeof(int32_t));
+                drwav_read_pcm_frames_s32(&wav_, wav_.totalPCMFrameCount, pSampleData);
+
+                for(int64_t index = 0, count =0 ; index < total_data; index+= 2, count++)
+                {
+                    //if ( index )
+
+                }
+                // At this point pSampleData contains every decoded sample as signed 32-bit PCM.
+                result = false;
             }
             drwav_uninit(&wav_);
         }
