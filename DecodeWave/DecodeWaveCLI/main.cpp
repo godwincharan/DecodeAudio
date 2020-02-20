cdf9b93036ec38f5f7cc6c124b075ce8a6ba50b0
Charan DSouza <godwincharan@gmail.com>
2020-02-21 00:30:11 +0100

    parsing the arguments.

--- DecodeWave/DecodeWaveCLI/main.cpp
+++ DecodeWave/DecodeWaveCLI/main.cpp
@@ -1,6 +1,7 @@
 #include <DecodeWave.hpp>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 static void show_usage(std::string applicaton_name)
 {
@@ -15,9 +16,56 @@ static void show_usage(std::string applicaton_name)
 
 int main(int argc, char* argv[])
 {
+    if (argc < 3) {
+        show_usage(argv[0]);
+        return 1;
+    }
+
+    std::string file_path{};
+    uint16_t channel{0};
+    bool is_channel_provided = false;
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string arg = argv[i];
+        if ((arg == "-h") || (arg == "--help"))
+        {
+            show_usage(argv[0]);
+            return 0;
+        } 
+        else if ((arg == "-a") || (arg == "--audio"))
+        {
+            if (i + 1 < argc)
+            { 
+                file_path = argv[++i];
+            }
+            else
+            { 
+                std::cerr << "-a,--audio option requires one argument." << std::endl;
+                return 1;
+            }  
+        }
+        else if ((arg == "-c") || (arg == "--channel"))
+        {
+            if (i + 1 < argc)
+            { 
+                std::istringstream(argv[++i]) >> channel;
+                is_channel_provided = true;
+            }
+            else
+            { 
+                std::cerr << "-c,--channel option requires one argument." << std::endl;
+                return 1;
+            }  
+        }
+        else
+        {
+            show_usage(argv[0]);
+            return 1;
+        }
+    }
+
     using namespace decode_wave;
     std::shared_ptr<DecodeWave> decode_wave = std::make_shared<DecodeWave>();
-    std::string file_path("/Users/Charan/Documents/GitHub/DecodeWave/ProblemStatement/Decoding_WaveFiles/file_3.wav");
     auto return_value = decode_wave->OpenFile(file_path);
     if ( return_value)
     {
